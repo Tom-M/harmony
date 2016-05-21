b356d5fea305306bc82fb85f3361099edf8b4553
Tom Manford <Tom Manford>
2016-05-21 15:27:40 +0100

    Added a Line class as well as a MidiStatic Class which contains an (as yet untested) method for generating a line object from a midi file

---
+++ src/main/Line.java
@@ -0,0 +1,43 @@
+package main;
+
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Line {
+
+  // Ordered list of ordered quadruplets (timestamp, duration, pitch, velocity) representing the
+  // sequence of notes in this line
+  private List<Long> timestamps;
+  private List<Long> durations;
+  private List<Integer> pitches;
+  private List<Integer> velocities;
+
+  public Line() {
+    timestamps = new ArrayList<Long>();
+    durations = new ArrayList<Long>();
+    pitches = new ArrayList<Integer>();
+    velocities = new ArrayList<Integer>();
+  }
+
+  /**
+   * Adds a note to the line. Is configured to throw an exception if any of the input params are -1
+   * 
+   * @param timestamp The timestamp at which the note occurs (in ticks)
+   * @param duration The duration of the note
+   * @param pitch The pitch of the note                                              //????UNITS?????
+   * @param velocity The velocity of the note                                        //????UNITS?????
+   */
+  public void addNoteToLine(long timestamp, long duration, int pitch, int velocity) {
+
+    if (timestamp == -1 || duration == -1 || pitch == -1 || velocity == -1) {
+      throw new InvalidParameterException("Input should not be -1");
+    } else {
+      this.timestamps.add(timestamp);
+      this.durations.add(duration);
+      this.pitches.add(pitch);
+      this.velocities.add(velocity);
+    }
+  }
+
+}
---
+++ src/main/MidiStatic.java
@@ -0,0 +1,110 @@
+package main;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.InvalidParameterException;
+
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiEvent;
+import javax.sound.midi.MidiMessage;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.Sequence;
+import javax.sound.midi.ShortMessage;
+import javax.sound.midi.Track;
+
+/**
+ * 
+ * A class containing static methods for handling midi files
+ * 
+ * @author tmanf
+ *
+ */
+public class MidiStatic {
+
+  // Define the midi bytes which will be understood as switching a note on/off
+  public static final int NOTE_ON = 0x90;
+  public static final int NOTE_OFF = 0x80;
+
+
+
+  // A midi file can contain as many tracks as you like, each containing up to 16 channels (this is
+  // a way to get around the 16 channel limit)--see
+  // https://docs.oracle.com/javase/7/docs/api/javax/sound/midi/Track.html
+
+  /**
+   * Takes a string path (pointing to a midi file) and converts the midi data into a Line object
+   * 
+   * @param filepath The string filepath pointing to the midi file
+   * @return A Line object 
+   * @throws IOException 
+   * @throws InvalidMidiDataException 
+   */
+  public Line getMeoldyFromFile(String filepath) throws InvalidMidiDataException, IOException{
+   
+    //Check the filepath points (in principle) to a midi file
+    if (!filepath.endsWith(".midi") && !filepath.endsWith(".mid") ){
+      throw new InvalidParameterException("The filepath string must point to a midi file");
+    }
+    
+    File midiImport = new File(filepath);
+    
+    //Obtain a sequence from the midi file
+    Sequence sequence = MidiSystem.getSequence(midiImport);
+     
+    Line melody = new Line();
+    
+    //Iterate over the tracks in the sequence
+    for(Track track : sequence.getTracks()){
+      
+      // Look at each event in the track
+      for (int i = 0; i < track.size(); i++) {
+        
+        //These values will trigger an error in the event data could not be properly retrieved
+        long timestamp = -1;
+        long duration = -1;
+        int pitch = -1;
+        int velocity = -1;
+        
+        // Get the current midi event
+        MidiEvent event = track.get(i);
+        
+        // Get the timestamp for the event, in ticks
+        timestamp = event.getTick();  
+        
+        MidiMessage message = event.getMessage();
+        
+        //We are only interested in "Note On" and "Note Off" events, so we search for the On events then look ahead to the Off events
+        if (message instanceof ShortMessage) {
+          ShortMessage sm = (ShortMessage) message;
+          if (sm.getCommand() == NOTE_ON) {
+            pitch = sm.getData1();
+            velocity = sm.getData2();
+            
+            //Search ahead for the "Note Off" event
+            int j = i;
+            MidiEvent nextEvent = null; 
+            MidiMessage nextMessage = null;
+            ShortMessage sm2 = null; 
+            while (!(nextMessage instanceof ShortMessage) || !(sm2.getCommand() == NOTE_OFF)){
+              nextEvent = track.get(j+1);
+              nextMessage = nextEvent.getMessage(); 
+              sm2 = (ShortMessage) nextMessage; 
+              j++;
+              if (j > track.size()){
+                throw new IndexOutOfBoundsException("I have reached the end of the track without finding the 'Note Off' signal");
+              }
+            long endTime = nextEvent.getTick();
+            duration = endTime - timestamp;
+            }
+            
+            //Add the note to the melody line
+            melody.addNoteToLine(timestamp,  duration,  pitch,  velocity); 
+          }
+        } 
+      }  
+    }
+    return melody;
+  }
+
+}
