222cd7e16a12c31f770fe1e20cc4f738cfdef784
Tom Manford <Tom Manford>
2016-06-14 21:46:09 +0100

    Added a method to mutate a note's pitch between a specified range

--- src/main/Note.java
+++ src/main/Note.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.security.InvalidParameterException;
+
 /**
  * Note objects form the building blocks of line objects
  * 
@@ -20,7 +22,7 @@ public class Note {
    * @param duration The duration (in ticks) of this note
    * @param pitch The pitch of the note (see
    *        https://andymurkin.files.wordpress.com/2012/01/midi-int-midi-note-no-chart.jpg )
-   * @param velocity The velocity of the note 
+   * @param velocity The velocity of the note
    */
   public Note(long timestamp, long duration, int pitch, int velocity) {
     this.timestamp = timestamp;
@@ -59,6 +61,24 @@ public class Note {
 
   public void setVelocity(int velocity) {
     this.velocity = velocity;
+  }
+
+  /**
+   * Changes the pitch to any key between minKey and maxKey, inclusive, with equal probability
+   * 
+   * @param minKey The lower bound (inclusive) which the note may mutate between 
+   * @param maxKey The upper bound (inclusive) which the note may mutate between 
+   */
+  public void mutatePitch(int minKey, int maxKey) {
+    if (minKey<0 || maxKey <0 || minKey>127 || maxKey >127){
+      throw new InvalidParameterException("keys must be between 0<key<127. You have set minKey = "
+        +minKey+" and maxKey = "+maxKey);
+    }
+    if (minKey>maxKey) {
+      throw new InvalidParameterException("minKey cannot be greater than maxKey");
+    }
+    int newPitch = (int) Math.round((double) minKey + (maxKey - minKey)*Math.random());
+    this.setPitch(newPitch);
   }
 
 }
