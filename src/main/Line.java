dc1185658fb90aedfd0f77478f05ddc56e09615e
Tom Manford <Tom Manford>
2016-06-23 17:57:38 +0100

    Added slf4j logger to pom, changed formatting of pom as well. Added default values for maxPitch and minPitch outside the valid midi range. Added logging to Line.class methods so we are warned if getMaxPitch or getMinPitch is called when maxPitch and minPitch have not been set. Added a check to the constructor that the supplied maxPitch and minPitch params make sense

--- src/main/Line.java
+++ src/main/Line.java
@@ -5,9 +5,14 @@ import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.sound.midi.InvalidMidiDataException;
 
 public class Line {
+
+  Logger logger = LoggerFactory.getLogger(Line.class);
 
   // Ordered list of ordered quadruplets (timestamp, duration, pitch, velocity) representing the
   // sequence of notes in this line
@@ -17,10 +22,13 @@ public class Line {
   private int ticksPerBeat;
 
   /**
-   * These dictate the max/min pitch values between which notes in this line may sit
+   * These dictate the max/min pitch values between which notes in this line may sit. The defaults
+   * -1 lie outside the midi pitch range, so will throw an error if they are called to mutate a
+   * note. This is to make sure the lines which don't have assigned max/min pitch ranges (the melody
+   * line) cannot be mutated
    */
-  private int maxPitch;
-  private int minPitch;
+  private int maxPitch = -1;
+  private int minPitch = -1;
 
   /**
    * Creates a line object with metadata concerning tempo but no specific note data
@@ -64,6 +72,7 @@ public class Line {
    * @param template The template line
    * @param minPitch The minimum pitch
    * @param maxPitch The maximum pitch
+   * @throws InvalidParameterException if the pitches are invalid
    */
   public Line(Line template, int minPitch, int maxPitch) {
     this.ticksPerBeat = template.getTicksPerBeat();
@@ -72,8 +81,15 @@ public class Line {
     for (Note note : this.notes) {
       note.mutatePitch(minPitch, maxPitch);
     }
-    this.maxPitch = maxPitch;
-    this.minPitch = minPitch;
+    if (maxPitch < 128 && minPitch < 128 && maxPitch >= 0 && minPitch >= 0 && maxPitch > minPitch) {
+      this.maxPitch = maxPitch;
+      this.minPitch = minPitch;
+    } else {
+      logger.error("Attempted to create line with minPitch = " + minPitch + ", maxPitch = "
+          + maxPitch + ". We require minPitch >= 0, maxPitch <= 127 & maxPitch > minPitch.");
+      throw new InvalidParameterException("Attempted to create line with minPitch = " + minPitch + ", maxPitch = "
+          + maxPitch + ". We require minPitch >= 0, maxPitch <= 127 & maxPitch > minPitch.");
+    }
 
   }
 
@@ -217,6 +233,10 @@ public class Line {
    * @return the maxPitch
    */
   public int getMaxPitch() {
+    if (this.maxPitch == -1) {
+      logger.warn(
+          "maxPitch does not seem to have been set for this Line object, it seems to be a melody");
+    }
     return maxPitch;
   }
 
@@ -224,6 +244,10 @@ public class Line {
    * @return the minPitch
    */
   public int getMinPitch() {
+    if (this.minPitch == -1) {
+      logger.warn(
+          "minPitch does not seem to have been set for this Line object, it seems to be a melody");
+    }
     return minPitch;
   }
 
