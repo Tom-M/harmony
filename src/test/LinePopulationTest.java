96671d1f48645b92cf2d3b810df40697f6603d3d
Tom Manford <Tom Manford>
2016-06-25 22:05:12 +0100

    Formatting changes

--- src/test/LinePopulationTest.java
+++ src/test/LinePopulationTest.java
@@ -18,38 +18,39 @@ public class LinePopulationTest {
   @Test
   public void testAddNewLineWithMelodyAsTemplate() throws InvalidMidiDataException, IOException {
     Line testMelody = new Line("src/test/Resources/MidiStaticTest_Resource1.mid");
-    
+
     LinePopulation testPop = new LinePopulation(testMelody);
-    
+
     testPop.addNewLineWithMelodyAsTemplate(5, 10);
-    
+
     Assert.assertNotNull(testPop.getLineAtIndex(1));
-    
-    //Check that all the notes lie within the specified pitch boundaries
+
+    // Check that all the notes lie within the specified pitch boundaries
     for (Note note : testPop.getLineAtIndex(1).getNotes()) {
       Assert.assertTrue(note.getPitch() >= 5 && note.getPitch() <= 10);
     }
-    
-    //Check there are the expected number of notes
+
+    // Check there are the expected number of notes
     Assert.assertEquals(testPop.getLineAtIndex(1).getNotes().size(), testMelody.getNotes().size());
-    
+
   }
 
   @Test
   public void testGetPitchFitnessScore() throws InvalidMidiDataException, IOException {
     Line testMelody = new Line("src/test/Resources/MidiStaticTest_Resource1.mid");
-    
+
     LinePopulation testPop = new LinePopulation(testMelody);
-    
+
     testPop.addNewLineWithMelodyAsTemplate(5, 10);
-    
-    //In this case, all the notes are of the same length, so we expect the score to only be informed by a single note starting at the same time
+
+    // In this case, all the notes are of the same length, so we expect the score to only be
+    // informed by a single note starting at the same time
     int MelodyStartingPitch = testMelody.getNotes().get(0).getPitch();
     int HarmonyStartingPitch = testPop.getLineAtIndex(1).getNotes().get(0).getPitch();
     int steps = Math.abs(MelodyStartingPitch - HarmonyStartingPitch);
-    
+
     double consonanceScore = testPop.getPitchFitnessScore(1, 0);
-    
+
     if (steps == 0) {
       Assert.assertTrue(consonanceScore == 0.25);
     } else if (steps % 7 == 0 || steps % 12 == 0) {
@@ -59,9 +60,9 @@ public class LinePopulationTest {
     } else {
       Assert.assertTrue(consonanceScore == 0.0);
     }
-    
-    //TODO Add a test case for the more complex case with multiple notes on at the same time
-    
+
+    // TODO Add a test case for the more complex case with multiple notes on at the same time
+
   }
 
 }
